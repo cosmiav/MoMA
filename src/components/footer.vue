e844d587f938c7a34ee326878d5f3797313aab8d
wisnuwicaksana100@students.amikom.ac.id <wisnuwicaksana100@students.amikom.ac.id>
2024-07-07 21:13:48 +0700

    updat

---
+++ src/components/footer.vue
@@ -0,0 +1,56 @@
+<script setup>
+// Script setup for Vue 3
+</script>
+
+<template>
+  <footer
+    class="text-center"
+    style="background-color: #f9f9f9"
+    data-bs-theme="light"
+  >
+    <div class="container-fluid p-4 p-md-5">
+      <!-- Section: Text -->
+      <section class="mb-5">
+        <div
+          class="d-flex justify-content-start flex-column text-start w-50 mb-3"
+        >
+          <h3 class="fw-medium">
+            Join our mailing list for updates about our artists, exhibitions,
+            and more.
+          </h3>
+        </div>
+        <button
+          class="btn btn-outline-dark px-4 py-2 rounded-0 d-flex justify-content-start"
+        >
+          Subscribe
+        </button>
+      </section>
+      <!-- Section: Text -->
+
+      <!-- Section: Social -->
+
+      <!-- Section: Social -->
+
+      <div class="row d-flex justify-content-between">
+        <div class="col-12 col-md-6">
+          <div class="row">
+            <div class="col">About</div>
+            <div class="col">Staff</div>
+            <div class="col">Carrers</div>
+            <div class="col">Press</div>
+            <div class="col">Term</div>
+            <div class="col">Privacy</div>
+            <div class="col">Accessibility</div>
+          </div>
+        </div>
+        <div class="col-12 col-md-3">© 2024 The Museum of Modern Art</div>
+      </div>
+    </div>
+
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+  <!-- End of .container -->
+</template>
+
+<style></style>
---
+++ src/components/home/collection.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="card text-bg-dark border-0 rounded-0">
+    <div class="image-wrapper">
+      <img
+        src="https://images.unsplash.com/photo-1514905552197-0610a4d8fd73?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        class="card-img img-fluid border-0 rounded-0"
+        alt="..."
+      />
+      <div class="dark-overlay"></div>
+    </div>
+    <div class="overlay">
+      <div
+        class="card-img-overlay d-flex flex-row align-items-center justify-content-start text-center text-md-start ms-md-5"
+      >
+        <div>
+          <h1 class="card-title fw-bold d-none d-md-block">
+            Modernism Explored
+          </h1>
+          <h2 class="card-title fw-bold d-md-none">Modernism Explored</h2>
+          <!--des-->
+          <h4 class="card-text my-3 fw-medium d-none d-md-block">
+            Journey through the history of modernism, featuring works that
+            defined the movement.
+          </h4>
+          <p
+            class="card-text my-3 fw-medium d-md-none"
+            style="line-height: 1.2"
+          >
+            Journey through the history of modernism, featuring works that
+            defined the movement.
+          </p>
+          <p class="card-text">
+            <router-link
+              :to="{ name: 'Collection' }"
+              class="btn btn-outline-light px-4 py-2 rounded-0 mt-4"
+            >
+              Explore More
+            </router-link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.image-wrapper {
+  position: relative; /* Agar overlay bisa ditumpuk di atas gambar */
+  height: 400px; /* Atur tinggi maksimal */
+  overflow: hidden; /* Sembunyikan bagian gambar yang melebihi container */
+  display: flex;
+  align-items: center; /* Gambar akan di tengah secara vertikal */
+  justify-content: center; /* Gambar akan di tengah secara horizontal */
+}
+
+.image-wrapper img {
+  height: 100%; /* Gambar akan menyesuaikan tinggi container */
+  width: 100%; /* Lebar akan menyesuaikan secara proporsional */
+  object-fit: cover; /* Gambar akan dipotong agar menutupi container */
+}
+
+.dark-overlay {
+  position: absolute; /* Posisi overlay absolut di dalam .image-wrapper */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3); /* Warna overlay dengan transparansi */
+}
+
+@media (max-width: 768px) {
+  .image-wrapper {
+    height: 300px; /* Kurangi tinggi pada layar kecil */
+  }
+}
+</style>
