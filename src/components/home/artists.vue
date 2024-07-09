8ef567a694a925ccc71b0584c43681afac1b28c5
alhan arobar <sheptiyanagungrizmawan@students.amikom.ac.id>
2024-07-09 14:10:46 +0700

    Latest Update

---
+++ src/components/home/artists.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container text-center my-md-5">
+    <h1 class="fw-medium mx-md-5 d-none d-md-block">
+      Since 1929, Moma has represented the most significant artists and estates
+      of the 20th and 21st centuries.
+    </h1>
+    <h5 class="fw-medium mx-md-5 d-md-none">
+      Since 1929, Moma has represented the most significant artists and estates
+      of the 20th and 21st centuries.
+    </h5>
+    <router-link :to="{ name: 'Artists' }" class="d-block my-4 my-md-5">
+      <p>View Artists</p>
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+a:link {
+  color: #111111;
+  text-decoration: underline;
+}
+
+a:visited {
+  color: #585858;
+}
+
+a:hover {
+  color: #282828;
+  text-decoration: underline;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/home.vue";
+import Artists from "@/views/artists.vue";
+import ArtistsPage from "@/components/artists/artistsPage.vue";
+import Collection from "@/views/collection.vue";
+import CollectionPage from "@/components/collection/collectionPage.vue";
+import Exhibition from "@/views/exhibition.vue";
+import Archive from "@/components/exhibition/archive.vue";
+import ExhibitionPage from "@/components/exhibition/exhibitionPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/collection",
+    name: "Collection",
+    component: Collection,
+  },
+  {
+    path: "/collection/:id",
+    name: "CollectionPage",
+    component: CollectionPage,
+    props: true,
+  },
+  {
+    path: "/artists",
+    name: "Artists",
+    component: Artists,
+  },
+  {
+    path: "/artists/:id",
+    name: "ArtistsPage",
+    component: ArtistsPage,
+    props: true,
+  },
+  {
+    path: "/exhibition",
+    name: "Exhibition",
+    component: Exhibition,
+  },
+  {
+    path: "/exhibition/:id",
+    name: "ExhibitionPage",
+    component: ExhibitionPage,
+    props: true,
+  },
+  {
+    path: "/exhibition/archive",
+    name: "Archive",
+    component: Archive,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
